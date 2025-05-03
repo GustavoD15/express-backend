677cb89dcbc0dccf07fcf367b40a0b324bb14f29
Gustavo Diniz <95726855+GustavoD15@users.noreply.github.com>
2025-05-03 16:35:30 +0000

    feat: add register endpoint

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express, { response } from 'express';
+import userController from '../controller/user.controller.js';
+
+const router = express.Router();
+
+router.post('/register', userController.register);
+
+export default router;
