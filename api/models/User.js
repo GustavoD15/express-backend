39128f2d60c00c3b2ffaa83456b1c6320f492567
Gustavo Diniz <gustavo.dinizvb@gmail.com>
2025-05-18 18:36:56 -0300

    fix/fix-frontend-connect-and-better-error-mensages

--- api/models/User.js
+++ api/models/User.js
@@ -1,26 +1,32 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: [true, "O nome é obrigatório."]
-    },
-    email: {
-        type: String,
-        required: [true, "O email é obrigatório."],
-        unique: true,
-        lowercase: true,
-        match: [/^\S+@\S+\.\S+$/, "Por favor, forneça um endereço de e-mail válido."]
-    },
-    password: {
-        type: String,
-        required: [true, "A senha é obrigatória."],
-        minlength: [5, "A senha deve ter pelo menos 5 caracteres."],
-        select: false // Para não retornar a senha em queries por padrão
-    }
-}, { timestamps: true }); // Adiciona createdAt e updatedAt automaticamente
+  nome: {
+    type: String,
+    required: [true, "O nome é obrigatório."],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, "O email é obrigatório."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^\S+@\S+\.\S+$/,
+      "Por favor, forneça um endereço de e-mail válido."
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, "A senha é obrigatória."],
+    minlength: [5, "A senha deve ter pelo menos 5 caracteres."],
+    select: false // Não retorna a senha por padrão
+  }
+}, {
+  timestamps: true // Cria createdAt e updatedAt automaticamente
+});
 
 const User = mongoose.model("User", UserSchema);
 
 export default User;
-
